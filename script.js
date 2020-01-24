9a7106a6bb06113c234b482439e4816ec5510ab3
Jay's Macbook <45748435+JayMatsushiba@users.noreply.github.com>
2020-01-24 15:13:06 -0800

    finished drum machine

--- script.js
+++ script.js
@@ -17,16 +17,27 @@ var DrumMachine = function (_React$Component) {
         var _this = _possibleConstructorReturn(this, (DrumMachine.__proto__ || Object.getPrototypeOf(DrumMachine)).call(this, props));
 
         _this.state = {
-            currentInstrument: ''
+            currentInstrument: 'Welcome!'
         };
+        _this.displayInstrument = _this.displayInstrument.bind(_this);
         return _this;
     }
 
     // need to loop through drumArray and create DrumPad for each one 
 
+
     _createClass(DrumMachine, [{
+        key: 'displayInstrument',
+        value: function displayInstrument(selectedInstrument) {
+            this.setState({
+                currentInstrument: selectedInstrument
+            });
+        }
+    }, {
         key: 'render',
         value: function render() {
+            var _this2 = this;
+
             return (
                 // need to pass properties of each drumArray object to the DrumPad map
                 React.createElement(
@@ -35,9 +46,22 @@ var DrumMachine = function (_React$Component) {
                     React.createElement(
                         'h1',
                         null,
-                        'Drum Machine'
+                        'DRUM MACHINE'
                     ),
-                    React.createElement(DrumPadArray, { drumInfoArray: drumInfoArray })
+                    React.createElement(
+                        'h2',
+                        null,
+                        this.state.currentInstrument
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'container' },
+                        drumInfoArray.map(function (drumPad) {
+                            return React.createElement(DrumPad, { displayInstrument: _this2.displayInstrument,
+                                instrument: drumPad.instrument,
+                                key: drumPad.KC, id: drumPad.KC, keyCode: drumPad.KC, keyboardInput: drumPad.key, audio: drumPad.link });
+                        })
+                    )
                 )
             );
         }
@@ -46,29 +70,17 @@ var DrumMachine = function (_React$Component) {
     return DrumMachine;
 }(React.Component);
 
-var DrumPadArray = function DrumPadArray(_ref) {
-    var drumInfoArray = _ref.drumInfoArray;
-    return React.createElement(
-        'div',
-        null,
-        drumInfoArray.map(function (drumPad) {
-            return React.createElement(DrumPad, { key: drumPad.KC, id: drumPad.KC, keyCode: drumPad.KC, keyboardInput: drumPad.key, audio: drumPad.link });
-        })
-    );
-};
-
 var DrumPad = function (_React$Component2) {
     _inherits(DrumPad, _React$Component2);
 
     function DrumPad(props) {
         _classCallCheck(this, DrumPad);
 
-        // DrumPad needs to take props handed from DrumMachine, expecting key, link and KC properties
-        var _this2 = _possibleConstructorReturn(this, (DrumPad.__proto__ || Object.getPrototypeOf(DrumPad)).call(this, props));
+        var _this3 = _possibleConstructorReturn(this, (DrumPad.__proto__ || Object.getPrototypeOf(DrumPad)).call(this, props));
 
-        _this2.handleClick = _this2.handleClick.bind(_this2);
-        _this2.handleKeypress = _this2.handleKeypress.bind(_this2);
-        return _this2;
+        _this3.handleClick = _this3.handleClick.bind(_this3);
+        _this3.handleKeypress = _this3.handleKeypress.bind(_this3);
+        return _this3;
     }
 
     _createClass(DrumPad, [{
@@ -85,12 +97,24 @@ var DrumPad = function (_React$Component2) {
         key: 'handleClick',
         value: function handleClick() {
             $("#" + this.props.keyboardInput).trigger("play");
+
+            // removes active class from all other buttons, then adds active class to last button pressed
+            $(".drum-button").removeClass("active");
+            $('#' + this.props.keyboardInput + 1).addClass("active");
+
+            this.props.displayInstrument(this.props.instrument);
         }
     }, {
         key: 'handleKeypress',
         value: function handleKeypress(event) {
             if (event.keyCode === this.props.keyCode) {
                 $("#" + this.props.keyboardInput).trigger("play");
+
+                // removes active class from all other buttons, then adds active class to last button pressed
+                $(".drum-button").removeClass("active");
+                $('#' + this.props.keyboardInput + 1).addClass("active");
+
+                this.props.displayInstrument(this.props.instrument);
             }
         }
     }, {
@@ -101,7 +125,7 @@ var DrumPad = function (_React$Component2) {
                 { id: this.props.id, className: 'drum-pad', onClick: this.handleClick },
                 React.createElement(
                     'button',
-                    null,
+                    { className: 'drum-button', id: this.props.keyboardInput + 1 },
                     this.props.keyboardInput
                 ),
                 React.createElement('audio', { className: 'clip', id: this.props.keyboardInput, src: this.props.audio })
@@ -112,6 +136,6 @@ var DrumPad = function (_React$Component2) {
     return DrumPad;
 }(React.Component);
 
-var drumInfoArray = [{ key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81 }, { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87 }, { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69 }, { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65 }, { key: 'S', link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83 }, { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68 }, { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90 }, { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88 }, { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67 }];
+var drumInfoArray = [{ key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81, instrument: "Kick" }, { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87, instrument: "Snare" }, { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69, instrument: "Closed Hi-hat" }, { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65, instrument: "Hi-hat" }, { key: 'S', link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83, instrument: "Heater 3" }, { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68, instrument: "Rim" }, { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90, instrument: "Heater 4" }, { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88, instrument: "Heater 2" }, { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67, instrument: "Heater 1" }];
 
 ReactDOM.render(React.createElement(DrumMachine, null), document.getElementById('drum-machine'));
--- src/script.js
+++ src/script.js
@@ -6,35 +6,39 @@ class DrumMachine extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentInstrument: ''
+            currentInstrument: 'Welcome!'
         }
+        this.displayInstrument = this.displayInstrument.bind(this)
     }
 
     // need to loop through drumArray and create DrumPad for each one 
-    
+    displayInstrument(selectedInstrument) {
+        this.setState({
+            currentInstrument: selectedInstrument
+        })
+    }
+
     render() {
         return (
             // need to pass properties of each drumArray object to the DrumPad map
-            <div id="display">
-                <h1>Drum Machine</h1>
-                <DrumPadArray drumInfoArray={drumInfoArray}/>
+            <div id="display" >
+                <h1>DRUM MACHINE</h1>
+                <h2>{this.state.currentInstrument}</h2>
+                <div className="container">
+                    {drumInfoArray.map(drumPad => (
+                        <DrumPad displayInstrument={this.displayInstrument} 
+                        instrument={drumPad.instrument}
+                        key={drumPad.KC} id={drumPad.KC} keyCode={drumPad.KC} keyboardInput={drumPad.key} audio={drumPad.link} />
+                    ))}
+                </div>                
             </div>
         )
     }
 }
 
-const DrumPadArray = ({drumInfoArray}) => (
-    <div>
-        {drumInfoArray.map(drumPad => (
-            <DrumPad key={drumPad.KC} id={drumPad.KC} keyCode={drumPad.KC} keyboardInput={drumPad.key} audio={drumPad.link}/>
-        ))}
-    </div>
-);
-
 class DrumPad extends React.Component {
     constructor(props) {
         super(props)
-        // DrumPad needs to take props handed from DrumMachine, expecting key, link and KC properties
         this.handleClick = this.handleClick.bind(this);
         this.handleKeypress = this.handleKeypress.bind(this);
     }
@@ -48,17 +52,29 @@ class DrumPad extends React.Component {
 
     handleClick() {
         $("#"+this.props.keyboardInput).trigger("play");
+        
+        // removes active class from all other buttons, then adds active class to last button pressed
+        $(".drum-button").removeClass("active");
+        $('#'+this.props.keyboardInput + 1).addClass("active");
+
+        this.props.displayInstrument(this.props.instrument);
     }
     handleKeypress(event) {
         if(event.keyCode === this.props.keyCode) {
             $("#"+this.props.keyboardInput).trigger("play");
+
+            // removes active class from all other buttons, then adds active class to last button pressed
+            $(".drum-button").removeClass("active");
+            $('#'+this.props.keyboardInput + 1).addClass("active");
+
+            this.props.displayInstrument(this.props.instrument);
         }
     }
 
     render() {
         return(
             <div id={this.props.id} className="drum-pad" onClick={this.handleClick}>
-                <button>{this.props.keyboardInput}</button>
+                <button className="drum-button" id={this.props.keyboardInput + 1}>{this.props.keyboardInput}</button>
                 <audio className="clip" id={this.props.keyboardInput} src={this.props.audio}></audio>
             </div>
         )
@@ -67,15 +83,15 @@ class DrumPad extends React.Component {
 
 
 const drumInfoArray = [
-    {key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81},
-    {key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87},
-    {key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69},
-    {key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65},
-    {key: 'S', link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83},
-    {key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68},
-    {key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90},
-    {key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88},
-    {key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67}
+    {key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81, instrument: "Kick"},
+    {key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87, instrument: "Snare"},
+    {key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69, instrument: "Closed Hi-hat"},
+    {key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65, instrument: "Hi-hat"},
+    {key: 'S', link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83, instrument: "Heater 3"},
+    {key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68, instrument: "Rim"},
+    {key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90, instrument: "Heater 4"},
+    {key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88, instrument: "Heater 2"},
+    {key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67, instrument: "Heater 1"}
   ]
 
 ReactDOM.render(<DrumMachine />, document.getElementById('drum-machine'))
