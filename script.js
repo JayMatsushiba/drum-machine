3033f47f103ced773269d0165b9489609126b85c
JayMatsushiba <45748435+JayMatsushiba@users.noreply.github.com>
2020-01-24 00:05:57 -0800

    scripts

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 'use strict';
 
-// const drums = [
-//     {
-//         name: "Snare",
-//         sound: 'https://actions.google.com/sounds/v1/impacts/hit_with_metal_tray.ogg',
-//         keyTrigger: 'Q'
-//     },
-//     {
-//         name: "Hihat",
-//         sound: 'chick',
-//         keyTrigger: 'W'
-//     },
-//     {
-//         name: "Bass Drum",
-//         sound: 'https://actions.google.com/sounds/v1/impacts/reverberating_slam.ogg',
-//         keyTrigger: 'E'
-//     }
-// ]
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -26,19 +8,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var drum = {
-    name: "Snare",
-    sound: 'https://actions.google.com/sounds/v1/impacts/hit_with_metal_tray.ogg',
-    keyTrigger: 'Q'
-};
-
-var activeStyle = {
-    backgroundColor: 'red'
-};
-
-var inactiveStyle = {
-    backgroundColor: 'green'
-};
+var drumArray = [{ Q: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81 }, { W: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87 }, { E: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69 }, { A: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65 }, { S: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83 }, { D: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68 }, { Z: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90 }, { X: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88 }, { C: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67 }];
 
 var DrumPad = function (_React$Component) {
     _inherits(DrumPad, _React$Component);
@@ -48,32 +18,21 @@ var DrumPad = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (DrumPad.__proto__ || Object.getPrototypeOf(DrumPad)).call(this, props));
 
-        _this.state = {
-            style: inactiveStyle
+        _this.handleClick = function (e) {
+            console.log('you hit the snare');
         };
+
         _this.handleClick = _this.handleClick.bind(_this);
         return _this;
     }
 
     _createClass(DrumPad, [{
-        key: 'handleClick',
-        value: function handleClick() {
-            this.setState = {
-                style: activeStyle
-            };
-            console.log('clicked');
-        }
-    }, {
         key: 'render',
         value: function render() {
             return React.createElement(
                 'div',
-                null,
-                React.createElement(
-                    'button',
-                    { onClick: this.handleClick, style: this.state.style },
-                    'Press me and change colour'
-                )
+                { onClick: this.handleClick },
+                'Change colour'
             );
         }
     }]);
--- src/script.js
+++ src/script.js
@@ -1,56 +1,30 @@
 'use strict';
 
-// const drums = [
-//     {
-//         name: "Snare",
-//         sound: 'https://actions.google.com/sounds/v1/impacts/hit_with_metal_tray.ogg',
-//         keyTrigger: 'Q'
-//     },
-//     {
-//         name: "Hihat",
-//         sound: 'chick',
-//         keyTrigger: 'W'
-//     },
-//     {
-//         name: "Bass Drum",
-//         sound: 'https://actions.google.com/sounds/v1/impacts/reverberating_slam.ogg',
-//         keyTrigger: 'E'
-//     }
-// ]
-
-const drum = {
-    name: "Snare",
-    sound: 'https://actions.google.com/sounds/v1/impacts/hit_with_metal_tray.ogg',
-    keyTrigger: 'Q'
-}
-
-const activeStyle = {
-    backgroundColor: 'red'
-}
-
-const inactiveStyle = {
-    backgroundColor: 'green'
-}
+const drumArray = [
+    {Q:  'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', KC: 81},
+    {W: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', KC: 87},
+    {E: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', KC: 69},
+    {A: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', KC: 65},
+    {S: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", KC: 83},
+    {D: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', KC: 68},
+    {Z: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', KC: 90},
+    {X: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', KC: 88},
+    {C: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', KC: 67}
+  ]
 
 class DrumPad extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            style: inactiveStyle
-        }
         this.handleClick = this.handleClick.bind(this)
     }
-    handleClick() {
-        this.setState = {
-            style: activeStyle
-        }
-        console.log('clicked');
+    handleClick = (e) => {
+        console.log('you hit the snare');
+        
     }
+
     render() {
         return(
-            <div>
-                <button onClick={this.handleClick} style={this.state.style}>Press me and change colour</button>
-            </div>
+            <div onClick={this.handleClick}>Change colour</div>
         )
     }
 }
